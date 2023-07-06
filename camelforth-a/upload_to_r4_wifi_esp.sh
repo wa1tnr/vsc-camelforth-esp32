17368945b06d3b7d272e6d8789deb1f5c64b4e81
wa1tnr <wa1tnr@users.noreply.github.com>
2023-07-06 21:36:09 +0000

    atomic upload

---
+++ camelforth-a/upload_to_r4_wifi_esp.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# ${1}  is /dev/ttyACM0   5 July 2023 at 21:04 UTC
+
+arduino-cli upload --fqbn arduino:renesas_uno:unor4wifi -p ${1}
+exit 0
+
+arduino-cli compile --fqbn arduino:renesas_uno:unor4wifi ${1}
+arduino-cli compile --fqbn arduino:renesas_uno:minima ${1}
