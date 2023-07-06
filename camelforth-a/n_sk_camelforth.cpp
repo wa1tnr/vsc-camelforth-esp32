b8b84293cb158909cdae3366915069d0fb29dee5
wa1tnr <wa1tnr@users.noreply.github.com>
2023-07-06 14:06:40 +0000

    working sort of

--- camelforth-a/n_sk_camelforth.cpp
+++ camelforth-a/n_sk_camelforth.cpp
@@ -1,146 +1,148 @@
 /* n_sk_camelforth.cpp */
-/* Tue  4 Jul 15:25:40 UTC 2023 */
+/* Thu  6 Jul 02:47:55 UTC 2023 */
 
 #include "api/Common.h"
-// #include "USB/USBAPI.h"
 #include <Arduino.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-  void this_here();
-  void interpreter(void);
-  extern void this_here_now();
+void this_here();
+void interpreter(void);
+extern void this_here_now();
 
-  void print_the_address(unsigned int *p) {
-    // unsigned int *q = p;
+void print_message(char * message) {
+    char buffer[32];
+    snprintf(buffer, 31, "%s", message, '\000');
+    Serial.print(buffer);
+}
+
+void print_the_address(unsigned int *p) {
     char buffer[12];
     snprintf(buffer, 11, "\n%8x:", p, '\000');
     Serial.print(buffer);
-  }
+}
 
-  void print_each_number(unsigned int *p) {
-    // unsigned int* q = (unsigned int*) *p;
+void print_each_number(unsigned int *p) {
     char buffer[12];
     buffer[0] = '\000';
     snprintf(buffer, 11, "  %8X", *p, '\000');
     Serial.print(buffer);
-  }
+}
 
-  void print_dump_addr(unsigned char *p) {
+void print_dump_addr(unsigned char *p) {
     char buffer[24];
     buffer[0] = '\000';
-    snprintf(buffer, 23, "\n%8x:", p, '\000');  // address at beginning of 16-byte line
+    snprintf(buffer, 23, "\n%8x:", p,
+             '\000'); // address at beginning of 16-byte line
     Serial.print(buffer);
-    // printf("\n%8x:", (unsigned int)p);
-  }
+}
 
-  void print_dumped_byte(unsigned char *p) {
+
+void xprint_dumped_char(unsigned char q) {
+    unsigned char *r;
+    r = (unsigned char*) q;
+    char buffer[4];
+    buffer[0] = '\000';
+    snprintf(buffer, 3, "%c", r, '\000');
+    Serial.print(buffer);
+}
+
+void print_dumped_char(unsigned char *p) {
+    char buffer[24];
+    buffer[0] = '\000';
+    snprintf(buffer, 23, " %c", *p, '\000');
+    Serial.print(buffer);
+}
+
+void print_dumped_byte(unsigned char *p) {
     char buffer[24];
     buffer[0] = '\000';
     snprintf(buffer, 23, " %02x", *p, '\000');
-    // printf(" %02x", *p++);
     Serial.print(buffer);
-  }
+}
 
-  /*
-     sample run - DUMP word  - line count on TOS
+/*
+  sample run - DUMP word  - line count on TOS
 
-    HEX EEDDCCBB 20 40 60 CR .S CR
-    HEX 200027D4 30 - 40 DUMP CR .S
+  HEX EEDDCCBB 20 40 60 CR .S CR
+  HEX 200024D4 30 - 40 DUMP CR .S
 
-    specific to Arduino M0 Pro   as in git repo Tue  4 Jul 15:19:22 UTC 2023
-    CamelForth in C v0.1 - 14 Feb 2016
-    HEX EEDDCCBB 20 40 60 CR .S CR
+*/
 
-    200027d4:  EEDDCCBB        20        40        60
-
-    ok HEX 200027D4 30 - 40 DUMP CR .S
-    200027a4: 10 00 00 00 01 00 00 00 05 00 00 00 00 00 00 00
-    200027b4: ff ff ff ff ff ff ff ff 30 05 00 20 40 00 00 00
-    200027c4: a4 27 00 20 60 00 00 00 40 00 00 00 20 00 00 00
-    200027d4: bb cc dd ee 00 00 00 00 48 45 58 20 32 30 30 30
-
-    200027d4:  EEDDCCBB        20        40        60
-    ok
-  */
-
-  char getch(void) {
+char getch(void) {
     bool waiting_ch = 0;
-    for (int testing = 5; testing > 0; testing--) {
-      ;
-    }
+    // for (int testing = 5; testing > 0; testing--) { ; }
     while (!waiting_ch) {
-      bool waiting_now = Serial.available();
-      waiting_ch = waiting_now;
+        bool waiting_now = Serial.available();
+        waiting_ch = waiting_now;
     }
     if (waiting_ch) {
-      unsigned int ch = Serial.read();
-      return (unsigned int)ch;
+        unsigned int ch = Serial.read();
+        return (unsigned int)ch;
     }
-  }
+}
 
-  void putch(char c) {
-    Serial.write(c);
-    // printf("%c", c);
-    return;  // doesn't have to do anything
-  }
+void putch(unsigned int c) {
+    char *q;
+    q = (char *)c;
+    Serial.write(q);
+}
 
-  int getquery(void) {
+int getquery(void) {
     // return(UARTCharsAvail(UART0_BASE) != 0);
     return (0 != 0);
-  }
-
-  void blinker() {
-    for (int count = 2; count > 0; count--) {
-      digitalWrite(LED_BUILTIN, 1);
-      delay(90);
-      digitalWrite(LED_BUILTIN, 0);
-      delay(700);
-    }
-  }
-
-void make_it_reset() {
-   NVIC_SystemReset();
 }
+
+void blinker() {
+    // for (int count = 2; count > 0; count--) {
+        digitalWrite(LED_BUILTIN, 1);
+        delay(70);
+        digitalWrite(LED_BUILTIN, 0);
+        delay(400);
+    // }
+}
+
+void make_it_reset() { NVIC_SystemReset(); }
 
 #ifdef __cplusplus
 }
 #endif
 
-void talker() {
-  Serial.println("jobs");
-}
+void talker() { Serial.println("jobs"); }
 
 void trapped() {
-  for (int count = 7; count > 0; count--) {
-    blinker();
-  }
-  // talker();
-  interpreter();
-  while (-1)
-    ;
+    for (int count = 3; count > 0; count--) {
+        blinker();
+    }
+    // talker();
+    interpreter();
+    while (-1)
+        ;
 }
 
 void setup() {
-  delay(2200);
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(115200);
-  // Serial.print("\nCamelforth in C   ");
-  Serial.print("\nspecific to Arduino M0 Pro   ");
-  // SerialUSB.println("as in git repo Tue  4 Jul 15:41:21 UTC 2023 a");
-  trapped();
+    delay(2200);
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(115200);
+    Serial.print("\nspecific to Arduino Uno R4 WiFi");
+    Serial.print("    vsc-camelforth-uno-r4wifi  ");
+    Serial.println(
+        "\n                                 Thu  6 Jul 02:47:55 UTC 2023 c");
+    Serial.println("\n\n    rev 9b55d294   shred: e");
+    trapped();
 }
 
-void loop() {
-  ;
-}
+void loop() { ; }
 
 /*
-   Sketch uses 33060 bytes (12%) of program storage space. Maximum is 262144 bytes.
-   Global variables use 18744 bytes (57%) of dynamic memory, leaving 14024 bytes for local variables. Maximum is 32768 bytes.
+   REDO ME
+
+   Sketch uses 33060 bytes (12%) of program storage space. Maximum is 262144
+   bytes. Global variables use 18744 bytes (57%) of dynamic memory, leaving
+   14024 bytes for local variables. Maximum is 32768 bytes.
 */
 
-// lupine wolfe bat
+// lupine wolfe bat walrus
 // END.
