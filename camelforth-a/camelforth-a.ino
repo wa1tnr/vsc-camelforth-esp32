b4383060cccfb475c5f12900572438228085f3aa
wa1tnr <wa1tnr@users.noreply.github.com>
2023-12-25 01:46:47 +0000

    edits
    	modified:   camelforth-a.ino
    
    On branch uno-r4-wifi-b

--- camelforth-a/camelforth-a.ino
+++ camelforth-a/camelforth-a.ino
@@ -1,5 +1,12 @@
 // near line 882 forth.c
 
+// Sun 24 Dec 23:02:02 UTC 2023
+
+// The ESP32 on Wokwi is meant
+// as a stand-in for the
+// Uno R4 WiFi (does not
+// exist on wokwi).
+
 // Sun  2 Jul 16:18:33 UTC 2023
 
 // end
